36e5f30240473684c8b21787b2a41bb893e6dce1
rebizant <unknown>
2013-07-12 12:06:45 +0200

    test

---
+++ src/test/java/ImageReaderCreatorTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: rebizant
+ * Date: 28.05.13
+ * Time: 16:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImageReaderCreatorTest {
+}
